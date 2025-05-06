d63fa90d76f70d856042cea64c74c9920e50ed21
Lukas <lukas.hertel@tum.de>
2025-05-06 21:06:56 +0200

    Complete config types

--- src/types/config.rs
+++ src/types/config.rs
@@ -6,8 +6,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Size {
-    value: f64,
-    unit: String,
+    pub value: f64,
+    pub unit: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -98,11 +98,11 @@ pub struct FolderDeviceConfiguration {
 #[serde(rename_all = "camelCase")]
 pub struct VersioningConfiguration {
     #[serde(rename = "type")]
-    config_type: String,
-    params: HashMap<String, String>,
-    cleanup_interval_s: i64,
-    fs_path: String,
-    fs_type: FilesystemType,
+    pub config_type: String,
+    pub params: HashMap<String, String>,
+    pub cleanup_interval_s: i64,
+    pub fs_path: String,
+    pub fs_type: FilesystemType,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -181,30 +181,30 @@ pub enum Compression {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ObservedFolder {
-    time: chrono::DateTime<chrono::Utc>,
-    id: String,
-    label: String,
+    pub time: chrono::DateTime<chrono::Utc>,
+    pub id: String,
+    pub label: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct GuiConfiguration {
-    enabled: bool,
-    address: String,
-    unix_socket_permissions: String,
-    user: String,
-    password: String,
-    auth_mode: AuthMode,
-    // metrics_without_auth: bool,
+    pub enabled: bool,
+    pub address: String,
+    pub unix_socket_permissions: String,
+    pub user: String,
+    pub password: String,
+    pub auth_mode: AuthMode,
+    // pub metrics_without_auth: bool,
     #[serde(rename = "useTLS")]
-    use_tls: bool,
-    api_key: String,
-    insecure_admin_access: bool,
-    theme: String,
-    debugging: bool,
-    insecure_skip_hostcheck: bool,
-    insecure_allow_frame_loading: bool,
-    send_basic_auth_prompt: bool,
+    pub use_tls: bool,
+    pub api_key: String,
+    pub insecure_admin_access: bool,
+    pub theme: String,
+    pub debugging: bool,
+    pub insecure_skip_hostcheck: bool,
+    pub insecure_allow_frame_loading: bool,
+    pub send_basic_auth_prompt: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -217,16 +217,122 @@ pub enum AuthMode {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct LDAPConfiguration {}
+pub struct LDAPConfiguration {
+    pub address: String,
+    #[serde(rename = "bindDN")]
+    pub bind_dn: String,
+    pub transport: LDAPTransport,
+    pub insecure_skip_verify: bool,
+    #[serde(rename = "searchBaseDN")]
+    pub search_base_dn: String,
+    pub search_filter: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum LDAPTransport {
+    Plain,
+    NonTLS,
+    TLS,
+    StartTLS,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct OptionsConfiguration {}
+pub struct OptionsConfiguration {
+    pub listen_address: Vec<String>,
+    pub global_announce_servers: Vec<String>,
+    pub global_annouce_enabled: bool,
+    pub local_announce_enabled: bool,
+    pub local_announce_port: i64,
+    #[serde(rename = "localAnnounceMCAddr")]
+    pub local_announce_mc_addr: String,
+    pub max_send_kbps: i64,
+    pub max_recv_kbps: i64,
+    pub reconnection_interval_s: i64,
+    pub relays_enabled: bool,
+    pub relay_reconnect_interval_m: i64,
+    pub start_browser: bool,
+    pub nat_enabled: bool,
+    pub nat_lease_minutes: i64,
+    pub nat_renewal_minutes: i64,
+    pub nat_timeout_seconds: i64,
+    pub ur_accepted: i64,
+    pub ur_screen: i64,
+    pub ur_unique_id: String,
+    #[serde(rename = "urURL")]
+    pub ur_url: String,
+    pub ur_post_insecurely: bool,
+    pub ur_initial_dleay_s: i64,
+    pub auto_upgraade_interval_h: i64,
+    pub upgrade_to_pre_releases: bool,
+    pub keep_temporaries_h: i64,
+    pub cache_ignored_files: bool,
+    pub progress_update_inteval_s: i64,
+    pub limit_bandwidth_in_lan: bool,
+    pub min_home_disk_free: Size,
+    #[serde(rename = "releasesURL")]
+    pub releases_url: String,
+    pub always_local_nets: Vec<String>,
+    pub overwrite_remote_device_names_on_connect: bool,
+    pub temp_index_min_blocks: i64,
+    #[serde(rename = "unackedNotificationIDs")]
+    pub unacked_notifications_ids: Vec<String>,
+    pub traffic_class: i64,
+    pub set_low_priority: bool,
+    pub max_folder_concurrency: i64,
+    #[serde(rename = "crURL")]
+    pub cr_url: String,
+    pub crash_reporting_enabled: bool,
+    pub stun_keepalive_start_s: i64,
+    pub stun_keepalive_min_s: i64,
+    pub stun_servers: Vec<String>,
+    pub database_tuning: Tuning,
+    pub max_concurrent_incoming_requests_ki_b: i64,
+    #[serde(rename = "announceLANAddresses")]
+    pub announce_lan_addresses: bool,
+    pub send_full_index_on_upgrade: bool,
+    pub feature_flags: Vec<String>,
+    pub audit_enabled: bool,
+    pub audit_file: String,
+    pub connection_limit_enough: i64,
+    pub connection_limit_max: i64,
+    pub connection_priority_tcp_lan: i64,
+    pub connection_priority_quic_lan: i64,
+    pub connection_priority_tcp_wan: i64,
+    pub connection_priority_quic_wan: i64,
+    pub connection_priority_relay: i64,
+    pub connection_priority_upgrade_threshold: i64,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct ObservedDevice {}
+pub enum Tuning {
+    Small,
+    Large,
+    Auto,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct Defaults {}
+pub struct ObservedDevice {
+    pub time: chrono::DateTime<chrono::Utc>,
+    #[serde(rename = "deviceID")]
+    pub device_id: String,
+    pub name: String,
+    pub address: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Defaults {
+    pub folder: FolderConfiguration,
+    pub device: DeviceConfiguration,
+    pub ignores: Ignores,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Ignores {
+    lines: Vec<String>,
+}
