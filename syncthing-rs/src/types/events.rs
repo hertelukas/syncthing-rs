75ae5ae539b9b0bf7526b6f74ea4c8d60d9fb8c1
Lukas <lukas.hertel@tum.de>
2025-07-03 22:03:21 +0200

    fix(events): typo in file download progress

--- syncthing-rs/src/types/events.rs
+++ syncthing-rs/src/types/events.rs
@@ -272,9 +272,9 @@ pub struct ListenAddressChanged {
 pub struct FileDownloadProgress {
     pub total: u64,
     pub pulling: u64,
-    pub copied_from_original: u64,
+    pub copied_from_origin: u64,
     pub reused: u64,
-    pub copied_from_original_shifted: u64,
+    pub copied_from_origin_shifted: u64,
     pub copied_from_elsewhere: u64,
     pub pulled: u64,
     pub bytes_total: u64,
